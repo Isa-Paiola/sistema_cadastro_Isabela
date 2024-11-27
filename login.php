385c7c84e9c456beb033795a71f9d7627a500d3b
email <isabelapaiola6@gmail.com>
2024-11-27 13:44:10 -0300

    commit

--- login.php
+++ login.php
@@ -3,17 +3,17 @@ session_start();
 include('conexao.php');
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $usuario = $_POST ['usuario'];
+    $usuario = $_POST['usuario'];
     $senha = md5($_POST['senha']);
 
-    $sql = "SELECT * FROM usuarios WHERE usuario = '$usuario' AND senha = '$senha'";
+    $sql = "SELECT * FROM usuarios WHERE usuario = '$usuario' AND senha='$senha'";
     $result = $conn->query($sql);
 
-    if($result-> num_rows > 0){
+    if ($result->num_rows > 0){
         $_SESSION['usuario'] = $usuario;
         header('Location: index.php');
     }else{
-        $error = "Usuário ou senha inválidos.";
+        $error = "Usuario ou senha invalidos.";
     }
 }
 ?>
@@ -23,19 +23,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="style.css">
+    <title>Logint</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <div class="container" style= "width: 400px;">
+    <div class="container" style="width: 400px;">
         <h2>Login</h2>
         <form method="post" action="">
-            <label for="usuario">Usuário:</label>
+            <label for="usuario">Usuario:</label>
             <input type="text" name="usuario" required>
             <label for="senha">Senha:</label>
             <input type="password" name="senha" required>
-            <button type="submit" style="margin-bottom: 30px;">Entrar</button>
-            <?php if (isset($error)) echo "<p class='message error'>$error</p>";?>
+            <button type="submit" style="margin-bottom:30px;">Entrar</button>
+            <?php if(isset($error)) echo "<p class='message error'>$error</p>"; ?>
         </form>
     </div>
 </body>
